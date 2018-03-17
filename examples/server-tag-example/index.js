1d7a98254c5921de920f876666c04c50a947afe2
Matthew Constantine <designmc@gmail.com>
2018-03-17 13:25:08 -0700

    Add <LocalServer> example

--- examples/server-tag-example/index.js
+++ examples/server-tag-example/index.js
@@ -1,55 +1,11 @@
 import React from 'react'
-import { spawn } from 'child_process'
 import { Code } from '../../src'
-
-const processPromise = (command, args = [], { cwd, logData, logError }) =>
-  new Promise((resolve, reject) => {
-    const proc = spawn(command, args, { cwd })
-    logData(`Starting \`${command} ${args.join(' ')}\``)
-    proc.on('error', error => {
-      logError(`Failed to start subprocess. Error: ${error}`)
-      reject(error)
-    })
-    proc.stdout.on('data', logData)
-    proc.stderr.on('data', logError)
-    proc.on('close', code => {
-      if (!code) {
-        logData('Successfuly Exited')
-        resolve(code)
-      } else {
-        logError(`Process Failed with exit code: ${code}`)
-        reject(code)
-      }
-    })
-  })
-
-const makeLogger = prefix => data =>
-  console.log(`\n_| ${prefix} |_____________________________________\n${data}`)
-
-const install = outputPath =>
-  processPromise('npm', ['install'], {
-    cwd: outputPath,
-    logData: makeLogger('Install Process'),
-    logError: data => console.log(`${data}`),
-  })
-
-const start = outputPath =>
-  processPromise('npm', ['start'], {
-    cwd: outputPath,
-    logData: makeLogger('Server Process'),
-    logError: data => console.log(`${data}`),
-  })
+import LocalServer from './LocalServer'
 
 class App extends React.Component {
-  installThenStart(outputPath) {
-    install(outputPath).then(() => {
-      start(outputPath)
-    })
-  }
-
   render() {
     return (
-      <project didRender={this.installThenStart}>
+      <LocalServer port={9100}>
         <copy from="boilerplate/package.json" key="package.json" />
         <copy from="boilerplate/webpack.config.js" key="webpack.config" />
         <dir name="dist" key="dist">
@@ -102,7 +58,7 @@ class App extends React.Component {
             </file>
           </dir>
         </dir>
-      </project>
+      </LocalServer>
     )
   }
 }
